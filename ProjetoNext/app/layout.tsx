800316218cedcdbeb33e711ceea6c8749c40dff0
MauricioAlmeida01 <mauriciofernandesdeoliveira@gmail.com>
2026-04-29 13:06:54 -0300

    feat: adicionei a fonte do jeito "certo" no Next colocando no layout e alterei o globals.css

--- ProjetoNext/app/layout.tsx
+++ ProjetoNext/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outfit } from 'next/font/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +13,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const outfit = Outfit({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${outfit.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
