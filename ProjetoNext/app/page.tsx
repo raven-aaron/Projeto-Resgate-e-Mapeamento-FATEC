df75bcab9e9d04527897438819cd07b90dcef909
MauricioAlmeida01 <mauriciofernandesdeoliveira@gmail.com>
2026-04-29 11:34:11 -0300

    style: css dos botões

--- ProjetoNext/app/page.tsx
+++ ProjetoNext/app/page.tsx
@@ -10,21 +10,21 @@ export default function Home() {
   return (
     <div>
       <Container>
+        <h1 className={styles.tituloDestaque}>PERDI O MEU PET</h1> <br /><br />
         <Row>
           <Col>
-              <h1 className={styles.tituloDestaque}>PERDI O MEU PET</h1> <br /><br />
               <div className="row align-items-center">
                 <div className="col-md-7 item-1">
-                  <div className={sttextos}>
+                  <div className={styles.textos}>
                     <p> Por meio de um mapa interativo, é possível cadastrar animais que seus tutores perderam 
                     ou pets que foram encontrados, permitindo que as informações fiquem disponíveis de 
                     forma rápida e acessível para todos.</p> 
                   </div>
 
                   <div className="text-center">
-                    <Button variant='primary' onClick={() => router.push('/dashboard')}/*caminho registro-USER.html*/>PERDI MEU PET</Button>
+                    <a href="registro-USER.html" className={`btn ${styles.btnCustom}`}>PERDI MEU PET</a>
                     <br /><br />
-                    <Button variant="primary" onClick={() => router.push('/dashboard')} /*caminho registro-USER.html*/>ENCONTREI UM PET</Button>
+                    <a href="registro-USER.html" className={`btn ${styles.btnCustom}`}>ENCONTREI UM PET</a>
                   </div>
                 </div>
                 <div className="col-md-5 item-2 text-center">
