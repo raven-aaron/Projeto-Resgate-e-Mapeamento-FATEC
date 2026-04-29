820928caee06a4fb03f8d6ee211258f5d70d50f8
MauricioAlmeida01 <mauriciofernandesdeoliveira@gmail.com>
2026-04-29 12:04:36 -0300

    feat: Fiz o outro Section na Pagina inicial

--- ProjetoNext/app/page.tsx
+++ ProjetoNext/app/page.tsx
@@ -34,8 +34,29 @@ export default function Home() {
               </Col>
             </Row>
         </section>
-        
-        
+        <section className="container mt-5 mb-5">
+            <br /><br /><br />
+            <h1 className={`${styles.tituloDestaque}`}>O MAPA E RESGATES</h1> <br /><br />
+
+            <div className="row align-items-center flex-md-row-reverse">
+                <div className="col-md-7 item-4">
+                    <div className={styles.textos}>
+                        <p>Quando um animal é sinalizado no sistema, a equipe de responsável avalia a situação e realiza o resgate 
+                        quando necessário. Assim, garantimos que nenhum pet fique desamparado, seja para devolução ao tutor ou 
+                        para acolhimento temporário seguro.</p>
+                    </div>
+
+                     <div className="text-center">
+                        <a href="mapa-interativo.html" className={`btn ${styles.btnCustom}`}>ACESSE O MAPA</a>
+                    </div>
+                    
+                </div>
+
+                <div className="col-md-5 item-3 text-center">
+                    <img id="cachorrogato2" src="assets/img/cachorrogato2.jpg" className={`img-fluid ${styles.imgBordaPadrao}`} />
+                </div>
+            </div>
+        </section>
       </Container>
     </div>
   );
